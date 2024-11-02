e9cee80683e31338405d25d73287a360f2da915a
Pranav <pskonduru12+gh@gmail.com>
2024-11-02 13:47:14 -0700

    Tried manually updating button ip

--- Lab2_2021v/Lab2_AXI_Implementation/Lab2_AXI_Implementation.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
+++ Lab2_2021v/Lab2_AXI_Implementation/Lab2_AXI_Implementation.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2021.1.1 (win64) Build 3286242 Wed Jul 28 13:10:47 MDT 2021
-//Date        : Sat Nov  2 13:34:08 2024
+//Date        : Sat Nov  2 13:46:24 2024
 //Host        : DESKTOP-DRHKE68 running 64-bit major release  (build 9200)
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
